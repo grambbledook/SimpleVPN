05dea031ebf6b6301fb3227604c7f85249da4439
grambbledook <vodopyanovd@gmail.com>
2026-08-20 00:16:33 +0200

    implement keys.rs

--- src/protocol/keys.rs
+++ src/protocol/keys.rs
@@ -1,4 +1,4 @@
-use x25519_dalek::{PublicKey as X25519PublicKey, PrivateKey as X25519PrivateKey, StaticSecret as X25519SharedSecret};
+use x25519_dalek::{PublicKey as DalekPublicKey, StaticSecret as DalekStaticSecret};
 pub const PUBLIC_KEY_SIZE: usize = 32;
 pub const PRIVATE_KEY_SIZE: usize = 32;
 pub const SHARED_SECRET_SIZE: usize = 32;
@@ -7,27 +7,35 @@ pub const TAI_64_NANO_TIMESTAMP_SIZE: usize = 12;
 pub const COOKIE_NONCE_SIZE: usize = 24;
 pub const COOKIE_SIZE: usize = 16;
 
-pub struct Privatekey([u8; PRIVATE_KEY_SIZE]);
-pub struct Publickey([u8; PUBLIC_KEY_SIZE]);
+pub struct PrivateKey([u8; PRIVATE_KEY_SIZE]);
+pub struct PublicKey([u8; PUBLIC_KEY_SIZE]);
 pub struct SharedSecret([u8; SHARED_SECRET_SIZE]);
 pub struct CookieNonce([u8; COOKIE_NONCE_SIZE]);
 
 pub type KeyError = std::io::Error;
 
-impl Privatekey {
+impl PrivateKey {
     pub fn generate() -> Self {
         let mut sk = [0u8; PRIVATE_KEY_SIZE];
         getrandom::fill(&mut sk).expect("failed to genersate the token");
-        Privatekey(sk)
+        PrivateKey(sk)
     }
 
-    pub fn public_key(&self) -> Publickey {
-       let sk =
-        Publickey([0u8; PUBLIC_KEY_SIZE])
+    pub fn public_key(&self) -> PublicKey {
+        let sk = DalekStaticSecret::from(self.0);
+        PublicKey(DalekPublicKey::from(&sk).to_bytes())
     }
 
-    pub fn shared_secret(&self, public_key: &Publickey) -> Result<SharedSecret, KeyError> {
-        return Result::Ok(SharedSecret([0u8; SHARED_SECRET_SIZE]));
+    pub fn shared_secret(&self, public_key: &PublicKey) -> Result<SharedSecret, KeyError> {
+        let sk = DalekStaticSecret::from(self.0);
+        let ss = sk.diffie_hellman(&DalekPublicKey::from(public_key.0));
+
+        if !ss.was_contributory() {
+            return Err(KeyError::new(
+                std::io::ErrorKind::InvalidData,
+                "bad input point: low order point",
+            ));
+        }
+        return Result::Ok(SharedSecret(ss.to_bytes()));
     }
 }
- 
